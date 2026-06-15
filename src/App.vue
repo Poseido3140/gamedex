4bcd6e795616c5c40f279a3fb5d1333911aa0baf
Owen <garcia.owen31@gmail.com>
2026-06-15 17:13:54 +0200

    étape 2 du projet

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <script setup>
 import {ref} from 'vue'
+import GameCard from './components/GameCard.vue';
 
-const score = ref(0)
+const jeux = ref([
+  { id: 1, nom: 'The Legend of Zelda: BOTW', note: 97, image: 'https://placehold.co/300x180?text=Zelda' },
+  { id: 2, nom: 'Elden Ring',                note: 96, image: 'https://placehold.co/300x180?text=Elden+Ring' },
+  { id: 3, nom: 'Minecraft',                 note: 93, image: 'https://placehold.co/300x180?text=Minecraft' },
+  { id: 4, nom: 'Hollow Knight',             note: 90, image: 'https://placehold.co/300x180?text=Hollow+Knight' },
+])
 
-function augmenterScore() {
-  score.value+=10
+function ajouterFavori(jeu) {
+  alert(jeu.nom + ' est ajouté aux favoris')
 }
 
-function reset() {
-  score.value=0
-}
 
 </script>
 
 <template>
-  <h1>Gamedex</h1>
-  <p>Score: {{score}}</p>
-  <button v-on:click="augmenterScore">+10</button>
-  <button v-on:click="reset">reset</button>
+  <h1>gamedex</h1>
+  <div class="grille">
+    <GameCard v-for="jeu in jeux" :key="jeu.id" :jeu="jeu" @favori="ajouterFavori"/>
+  </div>
 </template>
+
+<style scoped>
+.grille { display: grid; grid-template-columns: repeat(auto-fill, minmax(220px, 1fr)); gap: 1rem; }
+.carte  { border: 1px solid #ddd; border-radius: 12px; padding: 1rem; }
+.carte img { width: 100%; border-radius: 8px; }
+</style>
 
 <style scoped>
 h1 { color: #6d28d9; }
---
+++ src/components/GameCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+defineProps({
+  jeu: { type:Object, required: true}
+})
+
+const emit = defineEmits('favori')
+
+</script>
+
+<template>
+  <div class="carte">
+      <img :src="jeu.image" :alt="jeu.nom" />
+      <h2>{{ jeu.nom }}</h2>
+      <p :class="{green: jeu.note >= 90, orange: jeu.note < 90 }">Note : {{ jeu.note }}/100</p>
+      <button @click="emit('favori',jeu)">Favori</button>
+      <p v-if="jeu.note >= 95">Hit</p>
+    </div>
+</template>
+
+<style scoped>
+.green {color: green;}
+.orange {color: orange;}
+.carte { border: 1px solid #ddd; border-radius: 12px; padding: 1rem; }
+.carte img { width: 100%; border-radius: 8px; }
+</style>
