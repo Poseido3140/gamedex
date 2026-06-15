f4a1f7bdc0b044a7a02e73c70b6f8928061bc1d8
Owen <garcia.owen31@gmail.com>
2026-06-15 15:33:20 +0200

    GameDex

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from 'vue'
+
+const score = ref(0)
+
+function augmenterScore() {
+  score.value+=10
+}
+
+function reset() {
+  score.value=0
+}
+
+</script>
+
+<template>
+  <h1>Gamedex</h1>
+  <p>Score: {{score}}</p>
+  <button v-on:click="augmenterScore">+10</button>
+  <button v-on:click="reset">reset</button>
+</template>
+
+<style scoped>
+h1 { color: #6d28d9; }
+</style>
